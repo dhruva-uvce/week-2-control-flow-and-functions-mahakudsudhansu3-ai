6b3daac6412f502d4f3fd1276ececfeec1dbe7ac
Sudhansu Mahakud <mahakudsudhansu3@gmail.com>
2026-04-11 14:43:53 +0530

    Update Q03.py

--- Q03.py
+++ Q03.py
@@ -1,13 +1,7 @@
 # Q03. Multiplication Table (for loop)
-#
-# Ask the user for a positive integer n.
-# Print its multiplication table from 1 to 10.
-#
-# Sample Input:   Enter a number: 5
-# Sample Output:
-#   5 x 1 = 5
-#   5 x 2 = 10
-#   ...
-#   5 x 10 = 50
 
-# --- YOUR CODE HERE ---
+n = int(input("Enter a number: "))
+
+for i in range(1, 11):
+    result = n * i
+    print(n, "x", i, "=", result)
