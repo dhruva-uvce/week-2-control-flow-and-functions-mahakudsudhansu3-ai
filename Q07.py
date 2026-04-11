02588597690e17fbfc2b2d420ffe2f2b723b30c2
Sudhansu Mahakud <mahakudsudhansu3@gmail.com>
2026-04-11 14:56:30 +0530

    Update Q07.py

--- Q07.py
+++ Q07.py
@@ -1,12 +1,11 @@
-# Q07. Reverse a Number (while loop)
-#
-# Ask the user for a positive integer.
-# Print the reverse of the number using a while loop.
-#
-# Sample Input 1:   Enter a number: 1234
-# Sample Output 1:  Reversed: 4321
-#
-# Sample Input 2:   Enter a number: 5000
-# Sample Output 2:  Reversed: 5
+num = int(input("Enter a number: "))
+reversed_num = 0
 
-# --- YOUR CODE HERE ---
+temp_num = num 
+
+while temp_num > 0:
+    digit = temp_num % 10             
+    reversed_num = reversed_num * 10 + digit
+    temp_num //= 10                   
+
+print(f"Reversed: {reversed_num}")
